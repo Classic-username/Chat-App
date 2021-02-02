9725ce4f57441eeaa7872c0a499452673dd8f288
Joshua Draper <josh.drapergame@gmail.com>
2021-02-01 22:35:19 -0700

    temp icons, work needs to be done.

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 import './InfoBar.css';
 
-const InfoBar = () => {
+// import closeIcon from '../../icons/closeIcon'; these will be downloaded later, see 1:21:40, in the mean time...
+let onlineIcon = "ONLINE YAAAS";
+let closeIcon = "YOU LEAVIN?!";
+// import onlineIcon from '../../icons/onlineIcon';
 
+const InfoBar = () => {
+<div className="infoBar">
+    <div className="leftInnerContainer">
+        <img className="onlineIcon" src={onlineIcon} alt="online image"/>
+        <h3>roomName</h3>
+    </div>
+    <div className="RightInnerContainer">
+        <a href="/"><img src={closeIcon} alt="close image"/></a>
+    </div>
+</div>
 }
 
 export default InfoBar;
